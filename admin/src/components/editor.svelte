f4dbdf32341a43dfbefc3590682107e0c35bce96
skurtz97 <seankurtz75@gmail.com>
2022-04-22 18:23:08 -0500

    added prism removed highlightjs. added a default dev environment file.

--- admin/src/components/editor.svelte
+++ admin/src/components/editor.svelte
@@ -1,65 +1,104 @@
 <!-- https://css-tricks.com/creating-an-editable-textarea-that-supports-syntax-highlighted-code/ -->
-<script lang="ts">
-	let value = "";
-
-	function update() {
-		let textarea = document.querySelector<HTMLTextAreaElement>("#editing");
-		if (textarea) {
-			value = textarea.value;
-			let result = document.querySelector<HTMLSpanElement>("#highlighting-content");
-			if (result) {
-				result.innerHTML = value
-					.replace(new RegExp("&", "g"), "&")
-					.replace(new RegExp("<", "g"), "<"); /* Global RegExp */
-			}
-		}
-		scroll();
-	}
-
-	function scroll() {
-		/* Scroll result to scroll coords of event - sync with textarea */
-		let pre = document.querySelector<HTMLPreElement>("#highlighting-content");
-		let textarea = document.querySelector<HTMLTextAreaElement>("#highlighting-content");
-		// Get and set x and y
-		if (pre && textarea) {
-			pre.scrollTop = textarea.scrollTop;
-			pre.scrollLeft = textarea.scrollLeft;
-		}
-	}
+<script lang="ts" context="module">
+	import Prism from "prismjs";
 </script>
 
-<div class="editor">
-	<textarea id="editing" spellcheck="false" on:input={update} on:scroll={scroll} />
-	<pre id="highlighting" aria-hidden="true">
-        <code class="language-html" id="highlighting-content" />
-    </pre>
+<script lang="ts">
+	let editor: HTMLTextAreaElement;
+	let content: HTMLSpanElement;
+	let highlighter: HTMLPreElement;
+	let value = "";
+
+	function scroll() {
+		if (highlighter && editor) {
+			highlighter.scrollTop = editor.scrollTop;
+			highlighter.scrollLeft = editor.scrollLeft;
+		}
+	}
+	function update() {
+		if (content) {
+			if (value[value.length - 1] == "\n") {
+				value += " ";
+			}
+			content.innerHTML = value
+				.replace(new RegExp("&", "g"), "&")
+				.replace(new RegExp("<", "g"), "<"); /* Global RegExp */
+			Prism.highlightElement(content);
+			scroll();
+		}
+	}
+
+	// xpos and ypos are the width and height of the textarea elem. make them negative and add any margins / padding
+	// to get the value for the pre elements relative position in order to place them on top of eachother.
+</script>
+
+<div class="container">
+	<textarea
+		id="editor"
+		bind:value
+		on:input={update}
+		on:scroll={scroll}
+		spellcheck="false"
+		bind:this={editor}
+	/>
+	<pre id="highlighter" style:position="relative" bind:this={highlighter}>
+			<code class="language-javascript" id="content" bind:this={content} />
+		</pre>
 </div>
 
 <style>
-	#editing,
-	#highlighting,
-	#highlighting-content {
-		font-size: 15pt;
+	@import "prismjs/themes/prism.css";
+
+	.container {
+		flex: 1;
+		display: grid;
+	}
+	/* sizing, border, etc. */
+	#editor,
+	#highlighter {
+		grid-column: 1;
+		grid-row: 1;
+		margin: 0.5rem 0 0 0;
+		padding: 0.5rem;
+	}
+	/* typography */
+	#editor,
+	#highlighter,
+	#highlighter * {
+		font-size: large;
 		font-family: monospace;
 		line-height: 20pt;
 	}
-	#editing,
-	#highlighting {
-		position: absolute;
-		top: 0;
-		left: 0;
+	#highlighter {
+		z-index: 0;
+	}
+	#editor {
+		z-index: 1;
+	}
+
+	#editor {
+		color: transparent;
+		background: transparent;
+		caret-color: white;
+	}
+
+	#editor,
+	#highlighter {
 		overflow: auto;
 		white-space: nowrap;
 	}
-	#highlighting {
-		z-index: 0;
+
+	#editor {
+		resize: none;
 	}
-	/* Make textarea almost completely transparent */
-	#editing {
-		z-index: 1;
-		color: transparent;
-		background: transparent;
-		caret-color: white; /* Or choose your favorite color */
-		resize: none; /* No resize */
+
+	p code {
+		border-radius: 2px;
+		background-color: #eee;
+		color: #111;
 	}
+
+	/* position highlighter and editor on top of eachother */
+
+	/* move textare in front of highlighter */
 </style>
